a7d42f86e2e602362077eb841ff543c0f6513884
ZamZamGraphics <zamzamprintmedia@gmail.com>
2023-06-05 21:32:21 +0600

    Dark mode integrated

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
 import Head from "next/head";
 import Layout from "@/components/Layout";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/x-icon" href="/favicon.png" sizes="any" />
@@ -12,6 +13,6 @@ export default function App({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </ThemeProvider>
   );
 }
--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // 404 not found handler
 app.use((req, res, next) => {
